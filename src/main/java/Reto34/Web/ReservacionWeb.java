2473f923b0a956f2250799e65fcac5cb97185587
andyariza <92409616+andyariza@users.noreply.github.com>
2021-11-01 14:39:10 -0500

    Update ReservacionWeb.java

--- src/main/java/Reto34/Web/ReservacionWeb.java
+++ src/main/java/Reto34/Web/ReservacionWeb.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/Reservation")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class ReservacionWeb {
 
     @Autowired
