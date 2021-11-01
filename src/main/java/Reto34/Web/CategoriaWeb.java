0d68a193e58f4582aa6e6c5566a7596eb25eb9f5
andyariza <92409616+andyariza@users.noreply.github.com>
2021-11-01 14:36:29 -0500

    Update CategoriaWeb.java

--- src/main/java/Reto34/Web/CategoriaWeb.java
+++ src/main/java/Reto34/Web/CategoriaWeb.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/Category")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class CategoriaWeb {
 
     @Autowired
